66e8521e696af393ad7b0e983fbe090fb9552fa5
Alexander Potapenko <glider@google.com>
2017-07-25 18:46:59 +0200

    kmsan: fixup printk

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -372,6 +372,11 @@ __packed __aligned(4)
  */
 DEFINE_RAW_SPINLOCK(logbuf_lock);
 
+
+// TODO(glider): need to make sure we need logbuf_lock_is_locked at all.
+// Maybe things have changed in new Linux versions.
+bool logbuf_lock_is_locked = false;
+
 /*
  * Helper macros to lock/unlock logbuf_lock and switch between
  * printk-safe/unsafe modes.
@@ -380,10 +385,12 @@ DEFINE_RAW_SPINLOCK(logbuf_lock);
 	do {						\
 		printk_safe_enter_irq();		\
 		raw_spin_lock(&logbuf_lock);		\
+		logbuf_lock_is_locked = true;		\
 	} while (0)
 
 #define logbuf_unlock_irq()				\
 	do {						\
+		logbuf_lock_is_locked = false;		\
 		raw_spin_unlock(&logbuf_lock);		\
 		printk_safe_exit_irq();			\
 	} while (0)
@@ -392,10 +399,12 @@ DEFINE_RAW_SPINLOCK(logbuf_lock);
 	do {						\
 		printk_safe_enter_irqsave(flags);	\
 		raw_spin_lock(&logbuf_lock);		\
+		logbuf_lock_is_locked = true;		\
 	} while (0)
 
 #define logbuf_unlock_irqrestore(flags)		\
 	do {						\
+		logbuf_lock_is_locked = false;		\
 		raw_spin_unlock(&logbuf_lock);		\
 		printk_safe_exit_irqrestore(flags);	\
 	} while (0)
@@ -1832,12 +1841,15 @@ EXPORT_SYMBOL_GPL(vprintk_default);
  *
  * See the vsnprintf() documentation for format string extensions over C99.
  */
+// TODO(glider): move to header.
+extern void kmsan_vprintk_func(const char *fmt, va_list args);
 asmlinkage __visible int printk(const char *fmt, ...)
 {
 	va_list args;
 	int r;
 
 	va_start(args, fmt);
+	kmsan_vprintk_func(fmt, args);
 	r = vprintk_func(fmt, args);
 	va_end(args);
 
@@ -2196,6 +2208,7 @@ again:
 
 		printk_safe_enter_irqsave(flags);
 		raw_spin_lock(&logbuf_lock);
+		logbuf_lock_is_locked = true;
 		if (seen_seq != log_next_seq) {
 			wake_klogd = true;
 			seen_seq = log_next_seq;
@@ -2239,6 +2252,7 @@ skip:
 		}
 		console_idx = log_next(console_idx);
 		console_seq++;
+		logbuf_lock_is_locked = false;
 		raw_spin_unlock(&logbuf_lock);
 
 		stop_critical_timings();	/* don't trace print latency */
@@ -2255,6 +2269,7 @@ skip:
 	if (unlikely(exclusive_console))
 		exclusive_console = NULL;
 
+	logbuf_lock_is_locked = false;
 	raw_spin_unlock(&logbuf_lock);
 
 	up_console_sem();
@@ -2266,7 +2281,9 @@ skip:
 	 * flush, no worries.
 	 */
 	raw_spin_lock(&logbuf_lock);
+	logbuf_lock_is_locked = true;
 	retry = console_seq != log_next_seq;
+	logbuf_lock_is_locked = false;
 	raw_spin_unlock(&logbuf_lock);
 	printk_safe_exit_irqrestore(flags);
 
