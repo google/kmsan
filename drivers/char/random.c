3ec5f915eb2a2b879254d6a4d0a317ce531d11ba
Alexander Potapenko <glider@google.com>
2017-09-01 20:22:52 +0200

    kmsan: unpoison random data from chacha20_block()

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -862,6 +862,7 @@ static void _extract_crng(struct crng_state *crng,
 	if (arch_get_random_long(&v))
 		crng->state[14] ^= v;
 	chacha20_block(&crng->state[0], out);
+	kmsan_unpoison_shadow(out, CHACHA20_BLOCK_SIZE);
 	if (crng->state[12] == 0)
 		crng->state[13]++;
 	spin_unlock_irqrestore(&crng->lock, flags);
