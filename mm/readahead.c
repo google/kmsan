80babdcdebc1c5c6d3ba2d12ddd3cbeccd766ec9
Alexander Potapenko <glider@google.com>
2017-12-22 13:50:35 +0100

    kmsan: don't track pages allocated by __do_page_cache_readahead()

--- mm/readahead.c
+++ mm/readahead.c
@@ -180,7 +180,10 @@ int __do_page_cache_readahead(struct address_space *mapping, struct file *filp,
 		if (page && !radix_tree_exceptional_entry(page))
 			continue;
 
-		page = __page_cache_alloc(gfp_mask);
+		// The easiest way to handle these pages is to mark them
+		// untracked by KMSAN, assuming they're never used by anything
+		// else.
+		page = __page_cache_alloc(gfp_mask | __GFP_NO_KMSAN_SHADOW);
 		if (!page)
 			break;
 		page->index = page_offset;
