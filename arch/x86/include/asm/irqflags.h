fca0e18f428669eddafaad96a780f634a33eddce
Alexander Potapenko <glider@google.com>
2018-01-02 13:33:53 +0100

    kmsan: suppress false positives in assembly routines
    TODO(glider): handle the assembly in the compiler properly instead

--- arch/x86/include/asm/irqflags.h
+++ arch/x86/include/asm/irqflags.h
@@ -15,7 +15,8 @@
 
 extern inline unsigned long native_save_fl(void)
 {
-	unsigned long flags;
+	/// TODO(glider): false positive here.
+	unsigned long flags = 0;
 
 	/*
 	 * "=rm" is safe here, because "pop" adjusts the stack before
