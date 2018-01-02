a56e45ba3f301aee1b89019fcf50062fa4f86cb0
Alexander Potapenko <glider@google.com>
2018-01-02 12:43:52 +0100

    kmsan: route memcpy() calls into __msan_memcpy()

--- arch/x86/include/asm/string_64.h
+++ arch/x86/include/asm/string_64.h
@@ -33,7 +33,10 @@ extern void *memcpy(void *to, const void *from, size_t len);
 extern void *__memcpy(void *to, const void *from, size_t len);
 
 #ifndef CONFIG_FORTIFY_SOURCE
-#if (__GNUC__ == 4 && __GNUC_MINOR__ < 3) || __GNUC__ < 4
+#ifdef CONFIG_KMSAN
+extern void *__msan_memcpy(void *dst, const void *src, u64 n);
+#define memcpy(dst, src, len) __msan_memcpy(dst, src, len)
+#elif (__GNUC__ == 4 && __GNUC_MINOR__ < 3) || __GNUC__ < 4
 #define memcpy(dst, src, len)					\
 ({								\
 	size_t __len = (len);					\
