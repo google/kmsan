f31e382e40b7d57b8c1ebe1f21d5b3f51efae7b1
Alexander Potapenko <glider@google.com>
2018-03-13 12:46:59 +0100

    kmsan: unpoison pages allocated for reading in bio_copy_kern()
    SCSI devices overwrite these pages with data that we consider
    initialized. It's hard to find the exact place where it happens, so just
    unpoison the pages in advance.
    
    Sketch test case:
    
    int main()
    {
      int fd = open("/dev/sg0", O_RDWR);
      struct scsi_ioctl_command *cmd;
      cmd = calloc(1, sizeof(struct scsi_ioctl_command) + 128);
      cmd->outlen = 0x4a;
      ioctl(fd, /*SCSI_IOCTL_SEND_COMMAND*/1, cmd);
      fprintf(stderr, "data: %p\n", cmd->data);
      return 0;
    }

--- block/bio.c
+++ block/bio.c
@@ -24,6 +24,7 @@
 #include <linux/slab.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
+#include <linux/kmsan-checks.h>
 #include <linux/export.h>
 #include <linux/mempool.h>
 #include <linux/workqueue.h>
@@ -1520,6 +1521,11 @@ struct bio *bio_copy_kern(struct request_queue *q, void *data, unsigned int len,
 		page = alloc_page(q->bounce_gfp | gfp_mask);
 		if (!page)
 			goto cleanup;
+
+		// TODO(glider): if we're about to read data from a SCSI device,
+		// assume the page allocated for that is already initialized.
+		if (reading)
+			kmsan_unpoison_shadow(page_address(page), PAGE_SIZE);
 
 		if (!reading)
 			memcpy(page_address(page), p, bytes);
