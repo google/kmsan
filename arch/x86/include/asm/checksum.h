8113bcb69261bc6495c5d92ae5ecb7a332e211c9
Dmitry Vyukov <dvyukov@google.com>
2018-04-21 15:50:22 +0200

    kmsan: disable assembly checksums
    Checksum functions implemented in assembly produce
    large number of false positives.
    There are generic functions available that do not use
    assembly. Switch to them under KMSAN.

--- arch/x86/include/asm/checksum.h
+++ arch/x86/include/asm/checksum.h
@@ -1,6 +1,16 @@
 /* SPDX-License-Identifier: GPL-2.0 */
-#ifdef CONFIG_X86_32
-# include <asm/checksum_32.h>
+#ifndef CONFIG_KMSAN
+# ifdef CONFIG_X86_32
+#  include <asm/checksum_32.h>
+# else
+#  include <asm/checksum_64.h>
+# endif
 #else
-# include <asm/checksum_64.h>
+/*
+ * KMSAN poorly understands assembly, so it uses generic functions.
+ * TODO(dvyukov): properly support CONFIG_GENERIC_CSUM instead and
+ * enable it for KMSAN and KASAN.
+ */
+# include <linux/types.h>
+# include <asm-generic/checksum.h>
 #endif
