d6c351f832e331cde949c5e222ffc43ee1ca3147
Alexander Potapenko <glider@google.com>
2018-06-08 18:26:47 +0200

    kmsan: copy shadow/origins for COWed pages
    Fixes the following case:
    
    process_vm_readv(0xfd2, 0x20000440, 0x1, 0x200006c0, 0x1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0) = 0x2800
    clone(child_stack=0x20fbf000, flags=CLONE_FS) = 4052
    gettid()                                = 4050
    process_vm_readv(0xfd2, 0x20000440, 0x1, 0x200006c0, 0x1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0) = 0x2800

--- include/linux/kmsan.h
+++ include/linux/kmsan.h
@@ -69,6 +69,7 @@ int kmsan_alloc_page(struct page *page, unsigned int order, gfp_t flags);
 void kmsan_free_page(struct page *page, unsigned int order);
 void kmsan_split_page(struct page *page, unsigned int order);
 void kmsan_clear_user_page(struct page *page);
+void kmsan_copy_page_meta(struct page *dst, struct page *src);
 
 void kmsan_poison_slab(struct page *page, gfp_t flags);
 void kmsan_kmalloc_large(const void *ptr, size_t size, gfp_t flags);
@@ -99,6 +100,7 @@ static inline int kmsan_alloc_page(
 static inline void kmsan_free_page(struct page *page, unsigned int order) {}
 static inline void kmsan_split_page(struct page *page, unsigned int order) {}
 void kmsan_clear_user_page(struct page *page) {}
+void kmsan_copy_page_meta(struct page *dst, struct page *src) {}
 
 static inline void kmsan_poison_slab(struct page *page, gfp_t flags) {}
 static inline void kmsan_kmalloc_large(
