51132cb462331a3bf971c78eccd381db15011b3f
Alexander Potapenko <glider@google.com>
2018-06-14 15:15:50 +0200

    kmsan: fix off-by-one error in map_pages()
    fixes 0064ced63c1b76338697a193299e0f4496f4fb7c ("kmsan: don't track
    pages used for memory compaction")

--- mm/compaction.c
+++ mm/compaction.c
@@ -84,7 +84,6 @@ static void map_pages(struct list_head *list)
 
 		for (i = 0; i < nr_pages; i++) {
 			list_add(&page->lru, &tmp_list);
-			page++;
 #ifdef CONFIG_KMSAN
 			// TODO(glider): we may lose the metadata when copying
 			// something to these pages. Need to allocated shadow
@@ -93,6 +92,7 @@ static void map_pages(struct list_head *list)
 			page->shadow = NULL;
 			page->origin = NULL;
 #endif
+			page++;
 		}
 	}
 
