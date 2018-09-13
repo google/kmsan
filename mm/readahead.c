8dbc012422299ded5dba0784afac909239bd18c0
Alexander Potapenko <glider@google.com>
2018-09-13 11:03:36 +0200

    Revert "kmsan: don't track pages allocated by __do_page_cache_readahead()"
    This reverts commit 1054a1b1d404bd2c29416bbc8e63cd51f1ec88bb.
    It masks a real bug in reading /dev/nullb0

--- mm/readahead.c
+++ mm/readahead.c
@@ -190,10 +190,7 @@ unsigned int __do_page_cache_readahead(struct address_space *mapping,
 			continue;
 		}
 
-		// The easiest way to handle these pages is to mark them
-		// untracked by KMSAN, assuming they're never used by anything
-		// else.
-		page = __page_cache_alloc(gfp_mask | __GFP_NO_KMSAN_SHADOW);
+		page = __page_cache_alloc(gfp_mask);
 		if (!page)
 			break;
 		page->index = page_offset;
