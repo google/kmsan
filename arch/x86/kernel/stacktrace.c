a48bd8444477c5bafa3f7d85045a55b86c669ed1
Alexander Potapenko <glider@google.com>
2018-11-08 18:26:34 +0100

    kmsan: replace kmsan.allow_reporting with __attribute__
    Let's not expose KMSAN internals

--- arch/x86/kernel/stacktrace.c
+++ arch/x86/kernel/stacktrace.c
@@ -12,41 +12,78 @@
 #include <asm/stacktrace.h>
 #include <asm/unwind.h>
 
-void arch_stack_walk(stack_trace_consume_fn consume_entry, void *cookie,
-		     struct task_struct *task, struct pt_regs *regs)
+static int save_stack_address(struct stack_trace *trace, unsigned long addr,
+			      bool nosched)
+{
+	if (nosched && in_sched_functions(addr))
+		return 0;
+
+	if (trace->skip > 0) {
+		trace->skip--;
+		return 0;
+	}
+
+	if (trace->nr_entries >= trace->max_entries)
+		return -1;
+
+	trace->entries[trace->nr_entries++] = addr;
+	return 0;
+}
+
+static void noinline __save_stack_trace(struct stack_trace *trace,
+			       struct task_struct *task, struct pt_regs *regs,
+			       bool nosched)
 {
 	struct unwind_state state;
 	unsigned long addr;
 
-	if (regs && !consume_entry(cookie, regs->ip, false))
-		return;
-
-#ifdef CONFIG_KMSAN
-	/* PCs read off the stacks are frequently considered uninit. */
-	bool allow_reporting = current->kmsan.allow_reporting;
-	current->kmsan.allow_reporting = false;
-#endif
+	if (regs)
+		save_stack_address(trace, regs->ip, nosched);
 
 	for (unwind_start(&state, task, regs, NULL); !unwind_done(&state);
 	     unwind_next_frame(&state)) {
 		addr = unwind_get_return_address(&state);
-		if (!addr || !consume_entry(cookie, addr, false))
+		if (!addr || save_stack_address(trace, addr, nosched))
 			break;
 	}
 
-#ifdef CONFIG_KMSAN
-	current->kmsan.allow_reporting = allow_reporting;
-#endif
+	if (trace->nr_entries < trace->max_entries)
+		trace->entries[trace->nr_entries++] = ULONG_MAX;
 }
 
 /*
- * This function returns an error if it detects any unreliable features of the
- * stack.  Otherwise it guarantees that the stack trace is reliable.
- *
- * If the task is not 'current', the caller *must* ensure the task is inactive.
+ * Save stack-backtrace addresses into a stack_trace buffer.
  */
-int arch_stack_walk_reliable(stack_trace_consume_fn consume_entry,
-			     void *cookie, struct task_struct *task)
+void save_stack_trace(struct stack_trace *trace)
+{
+	trace->skip++;
+	__save_stack_trace(trace, current, NULL, false);
+}
+EXPORT_SYMBOL_GPL(save_stack_trace);
+
+void save_stack_trace_regs(struct pt_regs *regs, struct stack_trace *trace)
+{
+	__save_stack_trace(trace, current, regs, false);
+}
+
+void save_stack_trace_tsk(struct task_struct *tsk, struct stack_trace *trace)
+{
+	if (!try_get_task_stack(tsk))
+		return;
+
+	if (tsk == current)
+		trace->skip++;
+	__save_stack_trace(trace, tsk, NULL, true);
+
+	put_task_stack(tsk);
+}
+EXPORT_SYMBOL_GPL(save_stack_trace_tsk);
+
+#ifdef CONFIG_HAVE_RELIABLE_STACKTRACE
+
+static int __always_inline
+__save_stack_trace_reliable(struct stack_trace *trace,
+			    struct task_struct *task)
 {
 	struct unwind_state state;
 	struct pt_regs *regs;
@@ -60,7 +97,7 @@ int arch_stack_walk_reliable(stack_trace_consume_fn consume_entry,
 		if (regs) {
 			/* Success path for user tasks */
 			if (user_mode(regs))
-				return 0;
+				goto success;
 
 			/*
 			 * Kernel mode registers on the stack indicate an
@@ -83,7 +120,7 @@ int arch_stack_walk_reliable(stack_trace_consume_fn consume_entry,
 		if (!addr)
 			return -EINVAL;
 
-		if (!consume_entry(cookie, addr, false))
+		if (save_stack_address(trace, addr, false))
 			return -EINVAL;
 	}
 
@@ -95,8 +132,38 @@ int arch_stack_walk_reliable(stack_trace_consume_fn consume_entry,
 	if (!(task->flags & (PF_KTHREAD | PF_IDLE)))
 		return -EINVAL;
 
+success:
+	if (trace->nr_entries < trace->max_entries)
+		trace->entries[trace->nr_entries++] = ULONG_MAX;
+
 	return 0;
 }
+
+/*
+ * This function returns an error if it detects any unreliable features of the
+ * stack.  Otherwise it guarantees that the stack trace is reliable.
+ *
+ * If the task is not 'current', the caller *must* ensure the task is inactive.
+ */
+int save_stack_trace_tsk_reliable(struct task_struct *tsk,
+				  struct stack_trace *trace)
+{
+	int ret;
+
+	/*
+	 * If the task doesn't have a stack (e.g., a zombie), the stack is
+	 * "reliably" empty.
+	 */
+	if (!try_get_task_stack(tsk))
+		return 0;
+
+	ret = __save_stack_trace_reliable(trace, tsk);
+
+	put_task_stack(tsk);
+
+	return ret;
+}
+#endif /* CONFIG_HAVE_RELIABLE_STACKTRACE */
 
 /* Userspace stacktrace - based on kernel/trace/trace_sysprof.c */
 
@@ -122,15 +189,15 @@ copy_stack_frame(const void __user *fp, struct stack_frame_user *frame)
 	return ret;
 }
 
-void arch_stack_walk_user(stack_trace_consume_fn consume_entry, void *cookie,
-			  const struct pt_regs *regs)
+static inline void __save_stack_trace_user(struct stack_trace *trace)
 {
+	const struct pt_regs *regs = task_pt_regs(current);
 	const void __user *fp = (const void __user *)regs->bp;
 
-	if (!consume_entry(cookie, regs->ip, false))
-		return;
+	if (trace->nr_entries < trace->max_entries)
+		trace->entries[trace->nr_entries++] = regs->ip;
 
-	while (1) {
+	while (trace->nr_entries < trace->max_entries) {
 		struct stack_frame_user frame;
 
 		frame.next_fp = NULL;
@@ -140,8 +207,8 @@ void arch_stack_walk_user(stack_trace_consume_fn consume_entry, void *cookie,
 		if ((unsigned long)fp < regs->sp)
 			break;
 		if (frame.ret_addr) {
-			if (!consume_entry(cookie, frame.ret_addr, false))
-				return;
+			trace->entries[trace->nr_entries++] =
+				frame.ret_addr;
 		}
 		if (fp == frame.next_fp)
 			break;
@@ -149,3 +216,14 @@ void arch_stack_walk_user(stack_trace_consume_fn consume_entry, void *cookie,
 	}
 }
 
+void save_stack_trace_user(struct stack_trace *trace)
+{
+	/*
+	 * Trace user stack if we are not a kernel thread
+	 */
+	if (current->mm) {
+		__save_stack_trace_user(trace);
+	}
+	if (trace->nr_entries < trace->max_entries)
+		trace->entries[trace->nr_entries++] = ULONG_MAX;
+}
