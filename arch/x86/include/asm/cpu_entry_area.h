4cad9f047202db4451dc12e88875e025769a3536
Alexander Potapenko <glider@google.com>
2018-11-08 17:20:56 +0100

    kmsan: revert changes to arch/x86/include/asm/cpu_entry_area.h

--- arch/x86/include/asm/cpu_entry_area.h
+++ arch/x86/include/asm/cpu_entry_area.h
@@ -57,12 +57,6 @@ struct cpu_entry_area {
 #define CPU_ENTRY_AREA_TOT_SIZE	(CPU_ENTRY_AREA_SIZE * NR_CPUS)
 
 DECLARE_PER_CPU(struct cpu_entry_area *, cpu_entry_area);
-#ifdef CONFIG_KMSAN
-// The kernel can't get us struct pages corresponding to cpu_entry_area.
-// Thus use contiguous char arrays to store shadow and origin.
-DECLARE_PER_CPU(char[CPU_ENTRY_AREA_SIZE], cpu_entry_area_shadow);
-DECLARE_PER_CPU(char[CPU_ENTRY_AREA_SIZE], cpu_entry_area_origin);
-#endif
 
 extern void setup_cpu_entry_areas(void);
 extern void cea_set_pte(void *cea_vaddr, phys_addr_t pa, pgprot_t flags);
