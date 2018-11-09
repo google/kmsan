ab142fbb05537ae9d66efc7bbeb737864c69bfb9
Alexander Potapenko <glider@google.com>
2018-11-09 17:48:58 +0100

    kmsan: hooks for copy_to_user() and friends
    Memory that is copied from userspace must be unpoisoned.
    Before copying memory to userspace, check it and report an error if it
    contains uninitialized bits.
    
    Signed-off-by: Alexander Potapenko <glider@google.com>
    To: Alexander Potapenko <glider@google.com>
    Cc: Alexander Viro <viro@zeniv.linux.org.uk>
    Cc: Vegard Nossum <vegard.nossum@oracle.com>
    Cc: Dmitry Vyukov <dvyukov@google.com>
    Cc: Marco Elver <elver@google.com>
    Cc: Andrey Konovalov <andreyknvl@google.com>
    Cc: linux-mm@kvack.org
    ---
    
    v3:
     - fixed compilation errors reported by kbuild test bot
    
    v4:
     - minor variable fixes as requested by Andrey Konovalov
     - simplified code around copy_to_user() hooks
    
    v5:
     - fixed use of uninitialized value spotted by kbuild test robot <lkp@intel.com>
    
    v6:
     - switched to using instrumented.h
     - added kmsan headers where applicable
    
    Change-Id: I38428b9c7d1909b8441dcec1749b080494a7af99
    
    uaccess.h: iwyu: add KMSAN headers

--- arch/x86/include/asm/uaccess.h
+++ arch/x86/include/asm/uaccess.h
@@ -6,6 +6,7 @@
  */
 #include <linux/compiler.h>
 #include <linux/kasan-checks.h>
+#include <linux/kmsan-checks.h>
 #include <linux/string.h>
 #include <asm/asm.h>
 #include <asm/page.h>
@@ -131,6 +132,7 @@ extern int __get_user_bad(void);
 			ASM_CALL_CONSTRAINT				\
 		     : "0" (ptr), "i" (sizeof(*(ptr))));		\
 	(x) = (__force __typeof__(*(ptr))) __val_gu;			\
+	kmsan_unpoison_shadow((void *)&(x), sizeof(*(ptr)));		\
 	__builtin_expect(__ret_gu, 0);					\
 })
 
@@ -219,9 +221,12 @@ extern void __put_user_nocheck_8(void);
 	int __ret_pu;							\
 	void __user *__ptr_pu;						\
 	register __typeof__(*(ptr)) __val_pu asm("%"_ASM_AX);		\
+	__typeof__(*(ptr)) __vp_copy;					\
 	__chk_user_ptr(ptr);						\
 	__ptr_pu = (ptr);						\
 	__val_pu = (x);							\
+	__vp_copy = __val_pu;						\
+	kmsan_check_memory((void *)&(__vp_copy), sizeof(*(ptr)));	\
 	asm volatile("call __" #fn "_%P[size]"				\
 		     : "=c" (__ret_pu),					\
 			ASM_CALL_CONSTRAINT				\
@@ -275,7 +280,9 @@ extern void __put_user_nocheck_8(void);
 
 #define __put_user_size(x, ptr, size, label)				\
 do {									\
+	__typeof__(*(ptr)) __pus_val = x;				\
 	__chk_user_ptr(ptr);						\
+	kmsan_check_memory((void *)&(__pus_val), size);			\
 	switch (size) {							\
 	case 1:								\
 		__put_user_goto(x, ptr, "b", "iq", label);		\
@@ -331,6 +338,7 @@ do {									\
 	default:							\
 		(x) = __get_user_bad();					\
 	}								\
+	kmsan_unpoison_shadow((void *)&(x), size);			\
 } while (0)
 
 #define __get_user_asm(x, addr, itype, ltype, label)			\
