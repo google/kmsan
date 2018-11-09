53fe476748549d8d5185252756d5d11f4450a43f
Alexander Potapenko <glider@google.com>
2018-11-09 17:48:58 +0100

    kmsan: hooks for copy_to_user() and friends
    Memory that is copied from userspace must be unpoisoned.
    Before copying memory to userspace, check it and report an error if it
    contains uninitialized bits.
    
    Signed-off-by: Alexander Potapenko <glider@google.com>
    To: Alexander Potapenko <glider@google.com>
    Cc: Alexander Viro <viro@zeniv.linux.org.uk>
    Cc: Vegard Nossum <vegard.nossum@oracle.com>
    Cc: Dmitry Vyukov <dvyukov@google.com>
    Cc: Marco Elver <elver@google.com>
    Cc: Andrey Konovalov <andreyknvl@google.com>
    Cc: linux-mm@kvack.org
    ---
    
    v3:
     - fixed compilation errors reported by kbuild test bot
    
    v4:
     - minor variable fixes as requested by Andrey Konovalov
     - simplified code around copy_to_user() hooks
    
    v5:
     - fixed use of uninitialized value spotted by kbuild test robot <lkp@intel.com>
    
    v6:
     - switched to using instrumented.h
    
    Change-Id: I38428b9c7d1909b8441dcec1749b080494a7af99

--- include/linux/uaccess.h
+++ include/linux/uaccess.h
@@ -58,18 +58,26 @@
 static __always_inline __must_check unsigned long
 __copy_from_user_inatomic(void *to, const void __user *from, unsigned long n)
 {
-	instrument_copy_from_user(to, from, n);
+	unsigned long res;
+
+	instrument_copy_from_user_before(to, from, n);
 	check_object_size(to, n, false);
-	return raw_copy_from_user(to, from, n);
+	res = raw_copy_from_user(to, from, n);
+	instrument_copy_from_user_after(to, from, n, res);
+	return res;
 }
 
 static __always_inline __must_check unsigned long
 __copy_from_user(void *to, const void __user *from, unsigned long n)
 {
+	unsigned long res;
+
 	might_fault();
-	instrument_copy_from_user(to, from, n);
+	instrument_copy_from_user_before(to, from, n);
 	check_object_size(to, n, false);
-	return raw_copy_from_user(to, from, n);
+	res = raw_copy_from_user(to, from, n);
+	instrument_copy_from_user_after(to, from, n, res);
+	return res;
 }
 
 /**
@@ -109,8 +117,9 @@ _copy_from_user(void *to, const void __user *from, unsigned long n)
 	unsigned long res = n;
 	might_fault();
 	if (likely(access_ok(from, n))) {
-		instrument_copy_from_user(to, from, n);
+		instrument_copy_from_user_before(to, from, n);
 		res = raw_copy_from_user(to, from, n);
+		instrument_copy_from_user_after(to, from, n, res);
 	}
 	if (unlikely(res))
 		memset(to + (n - res), 0, res);
