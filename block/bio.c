2323e0ec26b38268c43a4b2675400131ec37a4d5
Alexander Potapenko <glider@google.com>
2018-11-09 17:40:43 +0100

    kmsan: unpoisoning buffers from devices etc.
    Change-Id: Id460e7a86ce564f1357469f53d0c7410ca08f0e9

--- block/bio.c
+++ block/bio.c
@@ -11,6 +11,7 @@
 #include <linux/slab.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
+#include <linux/kmsan-checks.h>
 #include <linux/export.h>
 #include <linux/mempool.h>
 #include <linux/workqueue.h>
@@ -900,6 +901,12 @@ static int __bio_iov_iter_get_pages(struct bio *bio, struct iov_iter *iter)
 			__bio_add_page(bio, page, len, offset);
 		}
 		offset = 0;
+		/* TODO(glider): these pages will be soon passed to block
+		 * device for reading, so consider them initialized.
+		 */
+		if (iov_iter_rw(iter) == READ)
+			kmsan_unpoison_shadow(page_address(page),
+						PAGE_SIZE);
 	}
 
 	iov_iter_advance(iter, size);
@@ -1274,11 +1281,14 @@ struct bio *bio_copy_user_iov(struct request_queue *q,
 
 			i++;
 		} else {
+			// TODO(glider): KMSAN doesn't track the pages
+			// allocated for bio here.
 			page = alloc_page(q->bounce_gfp | gfp_mask);
 			if (!page) {
 				ret = -ENOMEM;
 				break;
 			}
+			kmsan_ignore_page(page, /*order*/0);
 		}
 
 		if (bio_add_pc_page(q, bio, page, bytes, offset) < bytes) {
@@ -1573,6 +1583,11 @@ struct bio *bio_copy_kern(struct request_queue *q, void *data, unsigned int len,
 		page = alloc_page(q->bounce_gfp | gfp_mask);
 		if (!page)
 			goto cleanup;
+
+		// TODO(glider): if we're about to read data from a SCSI device,
+		// assume the page allocated for that is already initialized.
+		if (reading)
+			kmsan_unpoison_shadow(page_address(page), PAGE_SIZE);
 
 		if (!reading)
 			memcpy(page_address(page), p, bytes);
