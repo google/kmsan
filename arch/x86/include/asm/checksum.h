0aaff161c2c6b774142bb9204525b2412686198d
Alexander Potapenko <glider@google.com>
2018-11-23 17:19:43 +0100

    Revert "kmsan: disable assembly checksums"
    This reverts commit 918066b63ac9789082b15f052752ada9753296c8.

--- arch/x86/include/asm/checksum.h
+++ arch/x86/include/asm/checksum.h
@@ -1,16 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0 */
-#ifndef CONFIG_KMSAN
-# ifdef CONFIG_X86_32
-#  include <asm/checksum_32.h>
-# else
-#  include <asm/checksum_64.h>
-# endif
+#ifdef CONFIG_X86_32
+# include <asm/checksum_32.h>
 #else
-/*
- * KMSAN poorly understands assembly, so it uses generic functions.
- * TODO(dvyukov): properly support CONFIG_GENERIC_CSUM instead and
- * enable it for KMSAN and KASAN.
- */
-# include <linux/types.h>
-# include <asm-generic/checksum.h>
+# include <asm/checksum_64.h>
 #endif
