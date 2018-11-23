96c6c3194b1b6edafcf294f4f6987ed90efa10eb
Alexander Potapenko <glider@google.com>
2018-11-23 17:37:33 +0100

    net: kasan: kmsan: support CONFIG_GENERIC_CSUM on x86, enable it for KASAN/KMSAN
    This is needed to allow memory tools like KASAN and KMSAN see the
    memory accesses from the checksum code. Without CONFIG_GENERIC_CSUM the
    tools can't see memory accesses originating from handwritten assembly
    code.
    For KASAN it's a question of detecting more bugs, for KMSAN using the C
    implementation also helps avoid false positives originating from
    seemingly uninitialized checksum values.
    
    Signed-off-by: Alexander Potapenko <glider@google.com>
    To: Alexander Potapenko <glider@google.com>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Michal Simek <monstr@monstr.eu>
    Cc: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Cc: Vegard Nossum <vegard.nossum@oracle.com>
    Cc: Dmitry Vyukov <dvyukov@google.com>
    Cc: linux-mm@kvack.org
    ---
    
    Change-Id: I645e2c097253a8d5717ad87e2e2df6f6f67251f3

--- arch/x86/include/asm/checksum.h
+++ arch/x86/include/asm/checksum.h
@@ -1,6 +1,10 @@
 /* SPDX-License-Identifier: GPL-2.0 */
-#ifdef CONFIG_X86_32
-# include <asm/checksum_32.h>
+#ifdef CONFIG_GENERIC_CSUM
+# include <asm-generic/checksum.h>
 #else
-# include <asm/checksum_64.h>
+# ifdef CONFIG_X86_32
+#  include <asm/checksum_32.h>
+# else
+#  include <asm/checksum_64.h>
+# endif
 #endif
