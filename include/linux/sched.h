8b4a54111ee2545ab866797774216bdff2ef3248
Alexander Potapenko <glider@google.com>
2018-11-29 12:18:11 +0100

    kmsan: account for Dima's comments in the runtime

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1111,7 +1111,7 @@ struct task_struct {
 #endif
 
 #ifdef CONFIG_KMSAN
-	kmsan_thread_state              kmsan;
+	kmsan_task_state              kmsan;
 #endif
 
 #ifdef CONFIG_FUNCTION_GRAPH_TRACER
