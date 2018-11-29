459fd46942b58e65b65a9ec73872f46494ec02e7
Alexander Potapenko <glider@google.com>
2018-11-29 12:52:58 +0100

    kmsan: remove unnecessary cast

--- include/asm-generic/atomic-instrumented.h
+++ include/asm-generic/atomic-instrumented.h
@@ -22,9 +22,9 @@
 #endif
 
 #define KMSAN_CHECK_PARAM(name)	\
-	do {							\
-		if (KMSAN_CHECK_ATOMIC_PARAMS) 			\
-			kmsan_check_memory((void*)&(name), sizeof(name));	\
+	do {								\
+		if (KMSAN_CHECK_ATOMIC_PARAMS) 				\
+			kmsan_check_memory(&(name), sizeof(name));	\
 	} while (0)
 
 static __always_inline int atomic_read(const atomic_t *v)
