c7ef3d7a659babfccd6671ec4bbe821e021157a8
Alexander Potapenko <glider@google.com>
2018-12-03 09:59:44 +0100

    kmsan: introduce __no_sanitize_memory and __SANITIZE_MEMORY__

--- include/linux/compiler-clang.h
+++ include/linux/compiler-clang.h
@@ -21,9 +21,12 @@
 #define __SANITIZE_ADDRESS__
 #endif
 
-/* define __SANITIZE_MEMORY__ for KMSAN */
+/* KMSAN is a Clang tool, thus putting the defines here */
 #if __has_feature(memory_sanitizer)
-#define __SANITIZE_MEMORY__
+# define __SANITIZE_MEMORY__
+# define __no_sanitize_memory __attribute__((no_sanitize("kernel-memory")))
+#else
+# define __no_sanitize_memory
 #endif
 
 /*
