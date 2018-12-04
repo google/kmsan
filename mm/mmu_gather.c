d27194b8922dc58bea738b2cc8eaf92be8daf7fd
Alexander Potapenko <glider@google.com>
2018-12-04 13:43:42 +0100

    kmsan: disable instrumentation of certain functions
    kmsan: unpoison |tlb| in arch_tlb_gather_mmu()
    This is a hack to reduce stackdepot pressure.
    
    Change-Id: I684d23dac5a22eb0a4cea71993cb934302b17cea

--- mm/mmu_gather.c
+++ mm/mmu_gather.c
@@ -1,6 +1,7 @@
 #include <linux/gfp.h>
 #include <linux/highmem.h>
 #include <linux/kernel.h>
+#include <linux/kmsan-checks.h>
 #include <linux/mmdebug.h>
 #include <linux/mm_types.h>
 #include <linux/pagemap.h>
@@ -206,6 +207,15 @@ void tlb_flush_mmu(struct mmu_gather *tlb)
 void tlb_gather_mmu(struct mmu_gather *tlb, struct mm_struct *mm,
 			unsigned long start, unsigned long end)
 {
+	/* TODO(glider): this is a very bad idea.
+	 * struct mmu_gather contains 7 1-bit fields packed into a 32-bit
+	 * unsigned int value. The remaining 25 bits remain uninitialized and
+	 * are never used, but KMSAN updates the origin for them in
+	 * zap_pXX_range() in mm/memory.c, thus creating very long origin
+	 * chains. This is technically correct, but consumes too much memory.
+	 * Unpoisoning the whole structure will prevent creating such chains.
+	 */
+	kmsan_unpoison_shadow(tlb, sizeof(*tlb));
 	tlb->mm = mm;
 
 	/* Is it from 0 to ~0? */
