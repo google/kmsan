acac6365e64e9a9aa0f9613e1754cc3d6c45b438
Alexander Potapenko <glider@google.com>
2018-12-13 13:40:49 +0100

    kmsan: block: use SECTOR_SIZE instead of a magic constant

--- block/partition-generic.c
+++ block/partition-generic.c
@@ -665,7 +665,7 @@ unsigned char *read_dev_sector(struct block_device *bdev, sector_t n, Sector *p)
 		p->v = page;
 		retval = (unsigned char *)page_address(page) +  ((n & ((1 << (PAGE_SHIFT - 9)) - 1)) << 9);
 		/* Unpoison sector-sized chunk of memory coming from the device. */
-		kmsan_unpoison_shadow(retval, 512);
+		kmsan_unpoison_shadow(retval, SECTOR_SIZE);
 		return retval;
 fail:
 		put_page(page);
