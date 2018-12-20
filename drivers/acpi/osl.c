fb2cf5bc334d80e790f288a57c82e7341f06bfde
Alexander Potapenko <glider@google.com>
2018-12-20 15:41:11 +0100

    kmsan: s/kmsan_acpi_/kmsan_io/

--- drivers/acpi/osl.c
+++ drivers/acpi/osl.c
@@ -292,7 +292,7 @@ static void __iomem *acpi_map(acpi_physical_address pg_off, unsigned long pg_sz)
 	} else {
 		ret = acpi_os_ioremap(pg_off, pg_sz);
 	}
-	kmsan_acpi_map(ret, pg_sz);
+	kmsan_iomap(ret, pg_sz);
 	return ret;
 }
 
@@ -302,10 +302,10 @@ static void acpi_unmap(acpi_physical_address pg_off, void __iomem *vaddr)
 
 	pfn = pg_off >> PAGE_SHIFT;
 	if (should_use_kmap(pfn)) {
-		kmsan_acpi_unmap(vaddr, PAGE_SIZE);
+		kmsan_iounmap(vaddr, PAGE_SIZE);
 		kunmap(pfn_to_page(pfn));
 	} else {
-		kmsan_acpi_unmap(vaddr, /*size*/-1);
+		kmsan_iounmap(vaddr, /*size*/-1);
 		iounmap(vaddr);
 	}
 }
