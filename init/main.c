9b6ae8e088bde74e3826414612cddcc1d09b213f
Alexander Potapenko <glider@google.com>
2018-12-21 12:05:50 +0100

    kmsan: Changing existing files to enable KMSAN builds
    Change-Id: I37e0b7f2d2f2b0aeac5753ff9d6b411485fc374e

--- init/main.c
+++ init/main.c
@@ -32,6 +32,7 @@
 #include <linux/nmi.h>
 #include <linux/percpu.h>
 #include <linux/kmod.h>
+#include <linux/kmsan.h>
 #include <linux/vmalloc.h>
 #include <linux/kernel_stat.h>
 #include <linux/start_kernel.h>
@@ -554,6 +555,7 @@ static void __init mm_init(void)
 	 */
 	page_ext_init_flatmem();
 	report_meminit();
+	kmsan_initialize_shadow();
 	mem_init();
 	kmem_cache_init();
 	pgtable_init();
@@ -625,6 +627,7 @@ asmlinkage __visible void __init start_kernel(void)
 	sort_main_extable();
 	trap_init();
 	mm_init();
+	kmsan_initialize();
 
 	ftrace_init();
 
