aedfca6d36a6f6bc173528235fe52ca20d99ed1c
Alexander Potapenko <glider@google.com>
2018-12-21 12:01:58 +0100

    printk: add is_logbuf_locked()
    Change-Id: Ia5b9d6c8b6d218ae8ad5bd84e34219407e6dc267

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -389,6 +389,14 @@ __packed __aligned(4)
  */
 DEFINE_RAW_SPINLOCK(logbuf_lock);
 
+static int logbuf_lock_is_locked = 0;
+
+int is_logbuf_locked(void)
+{
+	return logbuf_lock_is_locked;
+}
+EXPORT_SYMBOL(is_logbuf_locked);
+
 /*
  * Helper macros to lock/unlock logbuf_lock and switch between
  * printk-safe/unsafe modes.
@@ -397,10 +405,12 @@ DEFINE_RAW_SPINLOCK(logbuf_lock);
 	do {						\
 		printk_safe_enter_irq();		\
 		raw_spin_lock(&logbuf_lock);		\
+		logbuf_lock_is_locked = 1;		\
 	} while (0)
 
 #define logbuf_unlock_irq()				\
 	do {						\
+		logbuf_lock_is_locked = 0;		\
 		raw_spin_unlock(&logbuf_lock);		\
 		printk_safe_exit_irq();			\
 	} while (0)
@@ -409,10 +419,12 @@ DEFINE_RAW_SPINLOCK(logbuf_lock);
 	do {						\
 		printk_safe_enter_irqsave(flags);	\
 		raw_spin_lock(&logbuf_lock);		\
+		logbuf_lock_is_locked = 1;		\
 	} while (0)
 
 #define logbuf_unlock_irqrestore(flags)		\
 	do {						\
+		logbuf_lock_is_locked = 0;		\
 		raw_spin_unlock(&logbuf_lock);		\
 		printk_safe_exit_irqrestore(flags);	\
 	} while (0)
@@ -2412,6 +2424,8 @@ again:
 
 		printk_safe_enter_irqsave(flags);
 		raw_spin_lock(&logbuf_lock);
+		logbuf_lock_is_locked = 1;
+
 		if (console_seq < log_first_seq) {
 			len = sprintf(text,
 				      "** %llu printk messages dropped **\n",
@@ -2459,6 +2473,7 @@ skip:
 		}
 		console_idx = log_next(console_idx);
 		console_seq++;
+		logbuf_lock_is_locked = 0;
 		raw_spin_unlock(&logbuf_lock);
 
 		/*
@@ -2486,6 +2501,7 @@ skip:
 
 	console_locked = 0;
 
+	logbuf_lock_is_locked = 0;
 	raw_spin_unlock(&logbuf_lock);
 
 	up_console_sem();
@@ -2497,7 +2513,9 @@ skip:
 	 * flush, no worries.
 	 */
 	raw_spin_lock(&logbuf_lock);
+	logbuf_lock_is_locked = 1;
 	retry = console_seq != log_next_seq;
+	logbuf_lock_is_locked = 0;
 	raw_spin_unlock(&logbuf_lock);
 	printk_safe_exit_irqrestore(flags);
 
