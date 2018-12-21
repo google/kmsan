ade06f5f8b479670619e69d4189133e888afc864
Alexander Potapenko <glider@google.com>
2018-12-21 11:51:12 +0100

    kmsan: drop our implementation of is_logbuf_locked()

--- mm/kmsan/kmsan.c
+++ mm/kmsan/kmsan.c
@@ -80,13 +80,6 @@ DEFINE_PER_CPU(char[CPU_ENTRY_AREA_SIZE], cpu_entry_area_origin);
 
 extern int oops_in_progress;
 
-extern bool logbuf_lock_is_locked;
-bool is_logbuf_locked(void)
-{
-	return logbuf_lock_is_locked;
-}
-EXPORT_SYMBOL(is_logbuf_locked);
-
 kmsan_context_state *task_kmsan_context_state(void)
 {
 	int cpu = smp_processor_id();
