6350b1c0b16e8b81504c462cda9748b13c780c18
Alexander Potapenko <glider@google.com>
2018-12-24 11:56:27 +0100

    kmsan: use per-cpu variable for runtime reentrancy counter
    Storing in_runtime in task_struct may lead to errors when accessing it
    from interrupts or scheduler code.
    Because interrupts are disabled between ENTER_RUNTIME/LEAVE_RUNTIME,
    keeping one counter per CPU should be enough.

--- mm/kmsan/kmsan.c
+++ mm/kmsan/kmsan.c
@@ -74,6 +74,8 @@ DEFINE_PER_CPU(int, kmsan_context_level);
 DEFINE_PER_CPU(int, kmsan_in_interrupt);
 DEFINE_PER_CPU(bool, kmsan_in_softirq);
 DEFINE_PER_CPU(bool, kmsan_in_nmi);
+DEFINE_PER_CPU(int, kmsan_in_runtime);
+DEFINE_PER_CPU(unsigned long, kmsan_runtime_last_caller);  // TODO(glider): debug-only
 
 DEFINE_PER_CPU(char[CPU_ENTRY_AREA_SIZE], cpu_entry_area_shadow);
 DEFINE_PER_CPU(char[CPU_ENTRY_AREA_SIZE], cpu_entry_area_origin);
