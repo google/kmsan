dac3696e40bffe6621eddf1c9cdf47f36279d47e
Andrey Konovalov <andreyknvl@google.com>
2019-01-17 19:25:34 +0100

    DO-NOT-SUBMIT: kcov: remote coverage support
    Signed-off-by: Andrey Konovalov <andreyknvl@google.com>
    Signed-off-by: Alexander Potapenko <glider@google.com>
    Cc: Vegard Nossum <vegard.nossum@oracle.com>
    Cc: Dmitry Vyukov <dvyukov@google.com>
    Cc: linux-mm@kvack.org
    
    ---
    
    Change-Id: I9d4631db13ce3c26abd7efba6ddbccd80cc14aae

--- include/uapi/linux/kcov.h
+++ include/uapi/linux/kcov.h
@@ -4,9 +4,20 @@
 
 #include <linux/types.h>
 
+struct kcov_remote_arg {
+	unsigned int	trace_mode;
+	unsigned int	area_size;
+	unsigned int	num_handles;
+	__u64		common_handle;
+	__u64		handles[0];
+};
+
+#define KCOV_REMOTE_MAX_HANDLES		0x10000
+
 #define KCOV_INIT_TRACE			_IOR('c', 1, unsigned long)
 #define KCOV_ENABLE			_IO('c', 100)
 #define KCOV_DISABLE			_IO('c', 101)
+#define KCOV_REMOTE_ENABLE		_IOW('c', 102, struct kcov_remote_arg)
 
 enum {
 	/*
@@ -31,5 +42,12 @@ enum {
 #define KCOV_CMP_CONST          (1 << 0)
 #define KCOV_CMP_SIZE(n)        ((n) << 1)
 #define KCOV_CMP_MASK           KCOV_CMP_SIZE(3)
+
+#define KCOV_REMOTE_HANDLE_USB  0x4242000000000000ull
+
+static inline __u64 kcov_remote_handle_usb(unsigned int bus)
+{
+	return KCOV_REMOTE_HANDLE_USB + (__u64)bus;
+}
 
 #endif /* _LINUX_KCOV_IOCTLS_H */
