6bb1574a543076d563fef349de0402a7738fd7c2
Andrey Konovalov <andreyknvl@google.com>
2019-01-17 19:26:34 +0100

    vhost, kcov: annotate vhost_worker
    Signed-off-by: Andrey Konovalov <andreyknvl@google.com>

--- drivers/vhost/vhost.h
+++ drivers/vhost/vhost.h
@@ -171,6 +171,9 @@ struct vhost_dev {
 	struct list_head pending_list;
 	wait_queue_head_t wait;
 	int iov_limit;
+#ifdef CONFIG_KCOV
+	u64 kcov_handle;
+#endif
 };
 
 void vhost_dev_init(struct vhost_dev *, struct vhost_virtqueue **vqs,
