53bbb296f3e1fd086b5e649a0aec271aa8a620ca
Alexander Potapenko <glider@google.com>
2019-05-15 17:06:46 +0200

    x86: kmsan: sync metadata pages on page fault
    KMSAN assumes shadow and origin pages for every allocated page are
    accessible. For pages between [VMALLOC_START, VMALLOC_END] those metadata
    pages start at KMSAN_VMALLOC_SHADOW_START and
    KMSAN_VMALLOC_ORIGIN_START, therefore we must sync a bigger memory
    region.
    
    Signed-off-by: Alexander Potapenko <glider@google.com>
    
    ---
    
    Link: https://linux-review.googlesource.com/id/Ia5bd541e54f1ecc11b86666c3ec87c62ac0bdfb8

--- arch/x86/mm/fault.c
+++ arch/x86/mm/fault.c
@@ -260,7 +260,7 @@ static noinline int vmalloc_fault(unsigned long address)
 }
 NOKPROBE_SYMBOL(vmalloc_fault);
 
-void arch_sync_kernel_mappings(unsigned long start, unsigned long end)
+void __arch_sync_kernel_mappings(unsigned long start, unsigned long end)
 {
 	unsigned long addr;
 
@@ -281,6 +281,26 @@ void arch_sync_kernel_mappings(unsigned long start, unsigned long end)
 			spin_unlock(pgt_lock);
 		}
 		spin_unlock(&pgd_lock);
+	}
+}
+
+void arch_sync_kernel_mappings(unsigned long start, unsigned long end)
+{
+	__arch_sync_kernel_mappings(start, end);
+	/*
+	 * KMSAN maintains two additional metadata page mappings for the
+	 * [VMALLOC_START, VMALLOC_END) range. These mappings start at
+	 * KMSAN_VMALLOC_SHADOW_START and KMSAN_VMALLOC_ORIGIN_START and
+	 * have to be synced together with the vmalloc memory mapping.
+	 */
+	if (IS_ENABLED(CONFIG_KMSAN) &&
+	    start >= VMALLOC_START && end < VMALLOC_END) {
+		__arch_sync_kernel_mappings(
+			start - VMALLOC_START + KMSAN_VMALLOC_SHADOW_START,
+			end - VMALLOC_START + KMSAN_VMALLOC_SHADOW_START);
+		__arch_sync_kernel_mappings(
+			start - VMALLOC_START + KMSAN_VMALLOC_ORIGIN_START,
+			end - VMALLOC_START + KMSAN_VMALLOC_ORIGIN_START);
 	}
 }
 
