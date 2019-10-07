6d1a207cb14fe9ce80a2f9805b21e63e3e2952e7
Alexander Potapenko <glider@google.com>
2019-10-07 17:10:19 +0200

    stackdepot: reserve 5 extra bits in depot_stack_handle_t
    Some users (currently only KMSAN) may want to use spare bits in
    depot_stack_handle_t. Let them do so and provide get_dsh_extra_bits()
    and set_dsh_extra_bits() to access those bits.
    
    Signed-off-by: Alexander Potapenko <glider@google.com>
    To: Alexander Potapenko <glider@google.com>
    Cc: Vegard Nossum <vegard.nossum@oracle.com>
    Cc: Dmitry Vyukov <dvyukov@google.com>
    Cc: linux-mm@kvack.org
    ---
    
    Change-Id: I23580dbde85908eeda0bdd8f83a8c3882ab3e012

--- include/linux/stackdepot.h
+++ include/linux/stackdepot.h
@@ -12,6 +12,11 @@
 #define _LINUX_STACKDEPOT_H
 
 typedef u32 depot_stack_handle_t;
+/*
+ * Number of bits in the handle that stack depot doesn't use. Users may store
+ * information in them.
+ */
+#define STACK_DEPOT_EXTRA_BITS 5
 
 depot_stack_handle_t stack_depot_save(unsigned long *entries,
 				      unsigned int nr_entries, gfp_t gfp_flags);
@@ -20,5 +25,8 @@ unsigned int stack_depot_fetch(depot_stack_handle_t handle,
 			       unsigned long **entries);
 
 unsigned int filter_irq_stacks(unsigned long *entries, unsigned int nr_entries);
+depot_stack_handle_t set_dsh_extra_bits(depot_stack_handle_t handle,
+					unsigned int bits);
+unsigned int get_dsh_extra_bits(depot_stack_handle_t handle);
 
 #endif
