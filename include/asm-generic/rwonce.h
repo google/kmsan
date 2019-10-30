c4f5d4ad8fe5f121cd04cb0ab37e7305348a21fa
Alexander Potapenko <glider@google.com>
2019-10-30 12:13:20 +0100

    kmsan: define READ_ONCE_NOCHECK()
    READ_ONCE_NOCHECK() is already used by KASAN to ignore memory accesses
    from e.g. stack unwinders.
    Define READ_ONCE_NOCHECK() for KMSAN so that it returns initialized
    values. This helps defeat false positives from leftover stack contents.
    
    Signed-off-by: Alexander Potapenko <glider@google.com>
    To: Alexander Potapenko <glider@google.com>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Vegard Nossum <vegard.nossum@oracle.com>
    Cc: Dmitry Vyukov <dvyukov@google.com>
    Cc: Marco Elver <elver@google.com>
    Cc: Andrey Konovalov <andreyknvl@google.com>
    Cc: linux-mm@kvack.org
    ---
    v3:
     - removed unnecessary #ifdef as requested by Mark Rutland
    v4:
     - added an #include as requested by Marco Elver
    v6:
     - READ_ONCE_NOCHECK() moved to include/asm-generic/rwonce.h
    
    Change-Id: Ib38369ba038ab3b581d8e45b81036c3304fb79cb

--- include/asm-generic/rwonce.h
+++ include/asm-generic/rwonce.h
@@ -25,6 +25,7 @@
 #include <linux/compiler_types.h>
 #include <linux/kasan-checks.h>
 #include <linux/kcsan-checks.h>
+#include <linux/kmsan-checks.h>
 
 /*
  * Yes, this permits 64-bit accesses on 32-bit architectures. These will
@@ -69,14 +70,14 @@ unsigned long __read_once_word_nocheck(const void *addr)
 
 /*
  * Use READ_ONCE_NOCHECK() instead of READ_ONCE() if you need to load a
- * word from memory atomically but without telling KASAN/KCSAN. This is
+ * word from memory atomically but without telling KASAN/KCSAN/KMSAN. This is
  * usually used by unwinding code when walking the stack of a running process.
  */
 #define READ_ONCE_NOCHECK(x)						\
 ({									\
 	compiletime_assert(sizeof(x) == sizeof(unsigned long),		\
 		"Unsupported access size for READ_ONCE_NOCHECK().");	\
-	(typeof(x))__read_once_word_nocheck(&(x));			\
+	KMSAN_INIT_VALUE((typeof(x))__read_once_word_nocheck(&(x)));			\
 })
 
 static __no_kasan_or_inline
