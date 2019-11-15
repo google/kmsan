639386570e3a3aeaf39858cdb7180bb66b8eee8c
Alexander Potapenko <glider@google.com>
2019-11-15 15:39:25 +0100

    kmsan: dma: drop annotations from dma_map_page_attrs()
    Looks like it's enough to unpoison the memory in dma_direct_map_page()
    (at least in the current setup)

--- include/linux/dma-mapping.h
+++ include/linux/dma-mapping.h
@@ -8,7 +8,6 @@
 #include <linux/err.h>
 #include <linux/dma-debug.h>
 #include <linux/dma-direction.h>
-#include <linux/kmsan-checks.h>
 #include <linux/scatterlist.h>
 #include <linux/bug.h>
 #include <linux/mem_encrypt.h>
@@ -282,7 +281,6 @@ static inline dma_addr_t dma_map_page_attrs(struct device *dev,
 	const struct dma_map_ops *ops = get_dma_ops(dev);
 	dma_addr_t addr;
 
-	kmsan_unpoison_shadow(page_address(page), size);
 	BUG_ON(!valid_dma_direction(dir));
 	if (dma_is_direct(ops))
 		addr = dma_direct_map_page(dev, page, offset, size, dir, attrs);
