dc6ad49f3ae5ca1527609777e464be23de9564e7
Alexander Potapenko <glider@google.com>
2019-12-16 11:21:01 +0100

    kmsan: printk: treat the result of vscnprintf() as initialized
    In vprintk_store(), vscnprintf() may return an uninitialized text_len
    value if any of its arguments are uninitialized. In that case KMSAN will
    report one or more errors in vscnprintf() itself, but it doesn't make
    much sense to track that value further, as it may trigger more errors in
    printk. Instead, we explicitly mark it as initialized.
    
    Signed-off-by: Alexander Potapenko <glider@google.com>
    To: Alexander Potapenko <glider@google.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: Eric Dumazet <edumazet@google.com>
    Cc: Wolfram Sang <wsa@the-dreams.de>
    Cc: Petr Mladek <pmladek@suse.com>
    Cc: Vegard Nossum <vegard.nossum@oracle.com>
    Cc: Dmitry Vyukov <dvyukov@google.com>
    Cc: Marco Elver <elver@google.com>
    Cc: Andrey Konovalov <andreyknvl@google.com>
    Cc: linux-mm@kvack.org
    Acked-by: Petr Mladek <pmladek@suse.com>
    Reviewed-by: Andrey Konovalov <andreyknvl@google.com>
    Change-Id: Id57455df4fa4101b04f354fa651c81761a4cf348
    
    ---
    
    This patch was split from "kmsan: call KMSAN hooks where needed", as
    requested by Andrey Konovalov. Petr Mladek has previously acked the
    printk part of that patch, hence the Acked-by above.
    
    v4:
     - split this patch away
    
    Change-Id: Ibed60b0bdd25f8ae91acee5800b5328e78e0735a

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -2007,6 +2007,14 @@ int vprintk_store(int facility, int level,
 		if (prb_reserve_in_last(&e, prb, &r, caller_id, LOG_LINE_MAX)) {
 			text_len = printk_sprint(&r.text_buf[r.info->text_len], reserve_size,
 						 facility, &lflags, fmt, args);
+
+			/*
+			 * If any of vscnprintf() arguments is uninitialized, KMSAN will report
+			 * one or more errors and also probably mark text_len as uninitialized.
+			 * Initialize |text_len| to prevent the errors from spreading further.
+			 */
+			text_len = KMSAN_INIT_VALUE(text_len);
+
 			r.info->text_len += text_len;
 
 			if (lflags & LOG_NEWLINE) {
@@ -2037,6 +2045,14 @@ int vprintk_store(int facility, int level,
 
 	/* fill message */
 	text_len = printk_sprint(&r.text_buf[0], reserve_size, facility, &lflags, fmt, args);
+
+	/*
+	 * If any of vscnprintf() arguments is uninitialized, KMSAN will report
+	 * one or more errors and also probably mark text_len as uninitialized.
+	 * Initialize |text_len| to prevent the errors from spreading further.
+	 */
+        text_len = KMSAN_INIT_VALUE(text_len);
+
 	if (trunc_msg_len)
 		memcpy(&r.text_buf[text_len], trunc_msg, trunc_msg_len);
 	r.info->text_len = text_len + trunc_msg_len;
