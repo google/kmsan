bc566509a8020e2f8a1acc348d77f2dac0afef64
Alexander Potapenko <glider@google.com>
2019-12-19 13:00:35 +0100

    kmsan: use kmsan_unpoison_shadow() in highmem.h

--- include/linux/highmem.h
+++ include/linux/highmem.h
@@ -256,8 +256,7 @@ static inline void copy_user_highpage(struct page *to, struct page *from,
 	vfrom = kmap_atomic(from);
 	vto = kmap_atomic(to);
 	copy_user_page(vto, vfrom, vaddr, to);
-	/* User pages don't have shadow, just clear the destination. */
-	kmsan_clear_page(page_address(to));
+	kmsan_unpoison_shadow(page_address(to), PAGE_SIZE);
 	kunmap_atomic(vto);
 	kunmap_atomic(vfrom);
 }
