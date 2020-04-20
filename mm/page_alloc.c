9535d09e7cffb82690b707a910db65f7ff31d49c
Alexander Potapenko <glider@google.com>
2020-04-20 09:39:34 +0200

    page_alloc: drop a call to kmsan_split_page()
    Signed-off-by: Alexander Potapenko <glider@google.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -3123,7 +3123,6 @@ void split_page(struct page *page, unsigned int order)
 	VM_BUG_ON_PAGE(PageCompound(page), page);
 	VM_BUG_ON_PAGE(!page_count(page), page);
 
-	kmsan_split_page(page, order);
 	for (i = 1; i < (1 << order); i++)
 		set_page_refcounted(page + i);
 	split_page_owner(page, order);
