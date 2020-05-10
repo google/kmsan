0c4fc8636d3bceccb09d45b2eaec952b5f40a249
Alexander Potapenko <glider@google.com>
2020-05-10 10:27:46 +0200

    kmsan: fix kmsan_handle_dma() prototype
    Signed-off-by: Alexander Potapenko <glider@google.com>

--- include/linux/kmsan.h
+++ include/linux/kmsan.h
@@ -336,8 +336,8 @@ static inline void kmsan_copy_to_user(
 	const void *to, const void *from, size_t to_copy, size_t left) {}
 
 static inline void kmsan_check_skb(const struct sk_buff *skb) {}
-static inline void kmsan_handle_dma(const void *address, size_t size,
-				    enum dma_data_direction direction) {}
+static inline void kmsan_handle_dma(struct page *page, size_t offset,
+				    size_t size, enum dma_data_direction dir) {}
 static inline void kmsan_handle_urb(const struct urb *urb, bool is_out) {}
 
 #endif
