eb15034ae62eb6835312fde55490bf3f00cd2d6d
Alexander Potapenko <glider@google.com>
2020-07-16 15:38:09 +0200

    fixup arch/x86/include/asm/checksum.h (CONFIG_GENERIC_CSUM)
    Signed-off-by: Alexander Potapenko <glider@google.com>

--- arch/x86/include/asm/checksum.h
+++ arch/x86/include/asm/checksum.h
@@ -1,9 +1,9 @@
 /* SPDX-License-Identifier: GPL-2.0 */
-#define  _HAVE_ARCH_COPY_AND_CSUM_FROM_USER 1
-#define HAVE_CSUM_COPY_USER
 #ifdef CONFIG_GENERIC_CSUM
 # include <asm-generic/checksum.h>
 #else
+# define  _HAVE_ARCH_COPY_AND_CSUM_FROM_USER 1
+# define HAVE_CSUM_COPY_USER
 # ifdef CONFIG_X86_32
 #  include <asm/checksum_32.h>
 # else
