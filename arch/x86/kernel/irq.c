e29d0831eddc75df182dcbf69e3d905ad8f14eca
Alexander Potapenko <glider@google.com>
2020-07-17 17:18:30 +0200

    kmsan: x86/asm: softirq: add KMSAN IRQ entry hooks
    Add assembly helpers to entry_64.S that invoke hooks from kmsan_entry.c and
    notify KMSAN about interrupts.
    Also call these hooks from kernel/softirq.c
    This is needed to switch between several KMSAN contexts holding function
    parameter metadata.
    
    Signed-off-by: Alexander Potapenko <glider@google.com>
    To: Alexander Potapenko <glider@google.com>
    Cc: Jens Axboe <axboe@kernel.dk>
    Cc: Andy Lutomirski <luto@kernel.org>
    Cc: Vegard Nossum <vegard.nossum@oracle.com>
    Cc: Dmitry Vyukov <dvyukov@google.com>
    Cc: Marco Elver <elver@google.com>
    Cc: Andrey Konovalov <andreyknvl@google.com>
    Cc: Christoph Hellwig <hch@lst.de>
    Cc: linux-mm@kvack.org
    Acked-by: Andrey Konovalov <andreyknvl@google.com>
    
    ---
    
    v4:
     - moved softirq changes to this patch
    
    v6:
     - reworked everything after irq handling has been rewritten to C
    
    Change-Id: I3037d51672fe69d09e588b27adb2d9fdc6ad3a7d
    
    Signed-off-by: Alexander Potapenko <glider@google.com>

--- arch/x86/kernel/irq.c
+++ arch/x86/kernel/irq.c
@@ -226,10 +226,13 @@ u64 arch_irq_stat(void)
 static __always_inline void handle_irq(struct irq_desc *desc,
 				       struct pt_regs *regs)
 {
+	kmsan_context_enter();
+	kmsan_unpoison_pt_regs(regs);
 	if (IS_ENABLED(CONFIG_X86_64))
 		run_irq_on_irqstack_cond(desc->handle_irq, desc, regs);
 	else
 		__handle_irq(desc, regs);
+	kmsan_context_exit();
 }
 
 /*
