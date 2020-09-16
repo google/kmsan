c530c8e8562dfbf3f3b492631890b75b9f3958ac
Alexander Potapenko <glider@google.com>
2020-09-16 17:50:57 +0200

    kmsan: be more consistent about unpoisoning pt_regs
    Signed-off-by: Alexander Potapenko <glider@google.com>

--- arch/x86/include/asm/idtentry.h
+++ arch/x86/include/asm/idtentry.h
@@ -49,6 +49,7 @@ static __always_inline void __##func(struct pt_regs *regs);		\
 									\
 __visible noinstr void func(struct pt_regs *regs)			\
 {									\
+	kmsan_unpoison_pt_regs(regs);					\
 	irqentry_state_t state = irqentry_enter(regs);			\
 									\
 	instrumentation_begin();					\
@@ -96,7 +97,7 @@ static __always_inline void __##func(struct pt_regs *regs,		\
 __visible noinstr void func(struct pt_regs *regs,			\
 			    unsigned long error_code)			\
 {									\
-	kmsan_unpoison_pt_regs(regs);						\
+	kmsan_unpoison_pt_regs(regs);					\
 	irqentry_state_t state = irqentry_enter(regs);			\
 									\
 	instrumentation_begin();					\
@@ -136,9 +137,9 @@ static __always_inline void __##func(struct pt_regs *regs,		\
 #define DEFINE_IDTENTRY_RAW(func)					\
 static __always_inline noinstr void __##func(struct pt_regs *regs); \
 __visible noinstr void func(struct pt_regs *regs) {	\
-	kmsan_unpoison_pt_regs(regs);						\
+	kmsan_unpoison_pt_regs(regs);				\
 	__##func(regs);						\
-}										\
+}								\
 static __always_inline noinstr void __##func(struct pt_regs *regs) \
 /**/
 
@@ -206,6 +207,7 @@ static __always_inline void __##func(struct pt_regs *regs, u8 vector);	\
 __visible noinstr void func(struct pt_regs *regs,			\
 			    unsigned long error_code)			\
 {									\
+	kmsan_unpoison_pt_regs(regs);					\
 	irqentry_state_t state = irqentry_enter(regs);			\
 									\
 	instrumentation_begin();					\
@@ -248,6 +250,7 @@ static void __##func(struct pt_regs *regs);				\
 									\
 __visible noinstr void func(struct pt_regs *regs)			\
 {									\
+	kmsan_unpoison_pt_regs(regs);					\
 	irqentry_state_t state = irqentry_enter(regs);			\
 									\
 	instrumentation_begin();					\
@@ -277,6 +280,7 @@ static __always_inline void __##func(struct pt_regs *regs);		\
 									\
 __visible noinstr void func(struct pt_regs *regs)			\
 {									\
+	kmsan_unpoison_pt_regs(regs);					\
 	irqentry_state_t state = irqentry_enter(regs);			\
 									\
 	instrumentation_begin();					\
