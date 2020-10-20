06a2d9cd719e295309596319684bdb93b0aa9faa
Alexander Potapenko <glider@google.com>
2020-10-20 14:25:37 +0200

    kmsan: retire kmsan_context_enter() and kmsan_context_exit()
    in_task() is a good replacement for them
    
    Signed-off-by: Alexander Potapenko <glider@google.com>

--- mm/kmsan/kmsan_entry.c
+++ mm/kmsan/kmsan_entry.c
@@ -13,22 +13,6 @@
 
 #include "kmsan.h"
 
-void kmsan_context_enter(void)
-{
-	int level = this_cpu_inc_return(kmsan_context_level);
-
-	BUG_ON(level >= KMSAN_NESTED_CONTEXT_MAX);
-}
-EXPORT_SYMBOL(kmsan_context_enter);
-
-void kmsan_context_exit(void)
-{
-	int level = this_cpu_dec_return(kmsan_context_level);
-
-	BUG_ON(level < 0);
-}
-EXPORT_SYMBOL(kmsan_context_exit);
-
 void kmsan_unpoison_pt_regs(struct pt_regs *regs)
 {
 	if (!kmsan_ready || kmsan_in_runtime() || !regs)
