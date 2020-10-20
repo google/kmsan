69f5c11dc5cd5bdf0cc29a1e7f731594c55cc2ef
Alexander Potapenko <glider@google.com>
2020-10-20 14:23:13 +0200

    softirq: kmsan: do not call kmsan_context_{enter,exit}
    ---
    
    REBASE: invoke_softirq was removed, so drop all changes in it.

--- kernel/softirq.c
+++ kernel/softirq.c
@@ -11,7 +11,6 @@
 
 #include <linux/export.h>
 #include <linux/kernel_stat.h>
-#include <linux/kmsan.h>
 #include <linux/interrupt.h>
 #include <linux/init.h>
 #include <linux/mm.h>
@@ -650,9 +649,7 @@ static void run_ksoftirqd(unsigned int cpu)
 		 * We can safely run softirq on inline stack, as we are not deep
 		 * in the task stack here.
 		 */
-		kmsan_context_enter();
 		__do_softirq();
-		kmsan_context_exit();
 		local_irq_enable();
 		cond_resched();
 		return;
