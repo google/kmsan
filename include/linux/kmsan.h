b96827e842c2acb985215189bd59cfe8bf3208ae
Alexander Potapenko <glider@google.com>
2020-11-11 14:27:33 +0100

    kmsan: core: remove GPL from file headers
    It is in SPDX-License comment already.

--- include/linux/kmsan.h
+++ include/linux/kmsan.h
@@ -2,12 +2,8 @@
 /*
  * KMSAN API for subsystems.
  *
- * Copyright (C) 2017-2019 Google LLC
+ * Copyright (C) 2017-2020 Google LLC
  * Author: Alexander Potapenko <glider@google.com>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
  *
  */
 #ifndef LINUX_KMSAN_H
