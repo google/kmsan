678479602e21389534df3743e39854db04cf4cf2
Alexander Potapenko <glider@google.com>
2020-11-11 14:30:03 +0100

    kmsan: core: remove GPL from file headers
    Signed-off-by: Alexander Potapenko <glider@google.com>

--- mm/kmsan/kmsan.c
+++ mm/kmsan/kmsan.c
@@ -2,12 +2,9 @@
 /*
  * KMSAN runtime library.
  *
- * Copyright (C) 2017-2019 Google LLC
+ * Copyright (C) 2017-2020 Google LLC
  * Author: Alexander Potapenko <glider@google.com>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
  */
 
 #include <asm/page.h>
