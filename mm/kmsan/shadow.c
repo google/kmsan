9740b14ee53f797aff97fd2f8db9ad52ec73db29
Alexander Potapenko <glider@google.com>
2022-03-17 19:26:30 +0100

    kmsan: shadow.c: add kmsan_free_page() impl

--- mm/kmsan/shadow.c
+++ mm/kmsan/shadow.c
@@ -267,7 +267,14 @@ void kmsan_alloc_page(struct page *page, unsigned int order, gfp_t flags)
 /* Called from mm/page_alloc.c */
 void kmsan_free_page(struct page *page, unsigned int order)
 {
-	return; // really nothing to do here. Could rewrite shadow instead.
+        if (!kmsan_enabled || kmsan_in_runtime())
+                return;
+        kmsan_enter_runtime();
+        kmsan_internal_poison_memory(page_address(page),
+                                     PAGE_SIZE << compound_order(page),
+                                     GFP_KERNEL,
+                                     KMSAN_POISON_CHECK | KMSAN_POISON_FREE);
+        kmsan_leave_runtime();
 }
 
 /* Called from mm/vmalloc.c */
