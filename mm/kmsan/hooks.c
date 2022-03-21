008d1e14b576a1afb5900d9076cf6baf4e2b92c9
Alexander Potapenko <glider@google.com>
2022-03-21 18:35:36 +0100

    kmsan: handle memory sent to/from USB
    Depending on the value of is_out kmsan_handle_urb() KMSAN either
    marks the data copied to the kernel from a USB device as initialized,
    or checks the data sent to the device for being initialized.
    
    Signed-off-by: Alexander Potapenko <glider@google.com>
    
    ---
    v2:
     -- move kmsan_handle_urb() implementation to this patch
    
    Link: https://linux-review.googlesource.com/id/Ifa67fb72015d4de14c30e971556f99fc8b2ee506

--- mm/kmsan/hooks.c
+++ mm/kmsan/hooks.c
@@ -17,6 +17,7 @@
 #include <linux/scatterlist.h>
 #include <linux/slab.h>
 #include <linux/uaccess.h>
+#include <linux/usb.h>
 
 #include "../internal.h"
 #include "../slab.h"
@@ -251,6 +252,22 @@ void kmsan_copy_to_user(void __user *to, const void *from, size_t to_copy,
 	user_access_restore(ua_flags);
 }
 EXPORT_SYMBOL(kmsan_copy_to_user);
+
+/* Helper function to check an URB. */
+void kmsan_handle_urb(const struct urb *urb, bool is_out)
+{
+	if (!urb)
+		return;
+	if (is_out)
+		kmsan_internal_check_memory(urb->transfer_buffer,
+					    urb->transfer_buffer_length,
+					    /*user_addr*/ 0, REASON_SUBMIT_URB);
+	else
+		kmsan_internal_unpoison_memory(urb->transfer_buffer,
+					       urb->transfer_buffer_length,
+					       /*checked*/ false);
+}
+EXPORT_SYMBOL(kmsan_handle_urb);
 
 static void kmsan_handle_dma_page(const void *addr, size_t size,
 				  enum dma_data_direction dir)
