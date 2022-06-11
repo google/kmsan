12ac2331e5c820a286d02589f2d50048fe31ec91
Alexander Potapenko <glider@google.com>
2022-06-11 15:14:01 +0200

    DO-NOT-UPSTREAM: namei: initialize parameters passed to step_into()
    Under certain circumstances initialization of `unsigned seq` and
    `struct inode *inode` passed into step_into() may be skipped.
    In particular, if the call to lookup_fast() in walk_component()
    returns NULL, and lookup_slow() returns a valid dentry, then the
    `seq` and `inode` will remain uninitialized until the call to
    step_into() (see [1] for more info).
    
    Right now step_into() does not use these uninitialized values,
    yet passing uninitialized values to functions is considered undefined
    behavior (see [2]). To fix that, we initialize `seq` and `inode` at
    definition.
    
    [1] https://github.com/ClangBuiltLinux/linux/issues/1648#issuecomment-1146608063
    [2] https://lore.kernel.org/linux-toolchains/CAHk-=whjz3wO8zD+itoerphWem+JZz4uS3myf6u1Wd6epGRgmQ@mail.gmail.com/
    
    Cc: Evgenii Stepanov <eugenis@google.com>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Marco Elver <elver@google.com>
    Cc: Nathan Chancellor <nathan@kernel.org>
    Cc: Nick Desaulniers <ndesaulniers@google.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Segher Boessenkool <segher@kernel.crashing.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Vitaly Buka <vitalybuka@google.com>
    Cc: linux-kernel@vger.kernel.org
    Cc: linux-toolchains@vger.kernel.org
    Signed-off-by: Alexander Potapenko <glider@google.com>

--- fs/namei.c
+++ fs/namei.c
@@ -1995,8 +1995,8 @@ static const char *handle_dots(struct nameidata *nd, int type)
 static const char *walk_component(struct nameidata *nd, int flags)
 {
 	struct dentry *dentry;
-	struct inode *inode;
-	unsigned seq;
+	struct inode *inode = NULL;
+	unsigned seq = 0;
 	/*
 	 * "." and ".." are special - ".." especially so because it has
 	 * to be able to know about the current root directory and
@@ -3393,8 +3393,8 @@ static const char *open_last_lookups(struct nameidata *nd,
 	struct dentry *dir = nd->path.dentry;
 	int open_flag = op->open_flag;
 	bool got_write = false;
-	unsigned seq;
-	struct inode *inode;
+	unsigned seq = 0;
+	struct inode *inode = NULL;
 	struct dentry *dentry;
 	const char *res;
 
