e725537efe5cfd41f5b07728faa5deb6aec0855b
Alexander Potapenko <glider@google.com>
2022-08-31 10:24:48 +0200

    kmsan: headers: split off struct definitions into kmsan_types.h

--- include/linux/kmsan.h
+++ include/linux/kmsan.h
@@ -12,7 +12,6 @@
 #include <linux/dma-direction.h>
 #include <linux/gfp.h>
 #include <linux/kmsan-checks.h>
-#include <linux/stackdepot.h>
 #include <linux/types.h>
 
 struct page;
@@ -22,29 +21,6 @@ struct scatterlist;
 struct urb;
 
 #ifdef CONFIG_KMSAN
-
-/* These constants are defined in the MSan LLVM instrumentation pass. */
-#define KMSAN_RETVAL_SIZE 800
-#define KMSAN_PARAM_SIZE 800
-
-struct kmsan_context_state {
-	char param_tls[KMSAN_PARAM_SIZE];
-	char retval_tls[KMSAN_RETVAL_SIZE];
-	char va_arg_tls[KMSAN_PARAM_SIZE];
-	char va_arg_origin_tls[KMSAN_PARAM_SIZE];
-	u64 va_arg_overflow_size_tls;
-	char param_origin_tls[KMSAN_PARAM_SIZE];
-	depot_stack_handle_t retval_origin_tls;
-};
-
-#undef KMSAN_PARAM_SIZE
-#undef KMSAN_RETVAL_SIZE
-
-struct kmsan_ctx {
-	struct kmsan_context_state cstate;
-	int kmsan_in_runtime;
-	bool allow_reporting;
-};
 
 /**
  * kmsan_task_create() - Initialize KMSAN state for the task.
